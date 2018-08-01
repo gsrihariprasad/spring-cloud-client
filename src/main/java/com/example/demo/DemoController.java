11ce83614decd60a6edbe8cd6cf106b2aab8831e
srihari <gsriharijava@gmail.com>
2018-08-01 21:54:56 +0530

    Spring boot

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class DemoController {
+	//@Value("#{${default.currency.limits}}")
+	@Value("${driverClassName}")
+	String driverClassName;
+	@Value("${url}")
+	String url;
+	@Value("${username}")
+	String username;
+	@Value("${password}")
+	String password;
+	
+	@RequestMapping(value="/db", method=RequestMethod.GET)
+	public String getDbDetails(){
+		Map<String, String> dbMap = new HashMap<>();
+		dbMap.put("driverClassName", driverClassName);
+		dbMap.put("url", url);
+		dbMap.put("username", username);
+		dbMap.put("password", password);
+		return dbMap.toString();
+	}
+}
